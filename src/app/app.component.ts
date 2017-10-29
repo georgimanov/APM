80fb133e2ecde6a0cb67f185fb16cb025f3293a4
Geogri Manov <georgimanov@gmail.com>
2017-10-29 10:29:23 +0200

    added product-list component - ts and with html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title: string = 'Acme Product Management';
+  nicknames: string[] = ['di', 'boo', 'punkeye'];
 }
