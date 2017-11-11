66cf702071ad409796b99bf0eca89a4d6bde5f88
Geogri Manov <georgimanov@gmail.com>
2017-11-11 12:01:58 +0200

    added product serive

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,60 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import {ProductService} from './product.service';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']    
+    styleUrls: ['./product-list.component.css']  
 })
 export class ProductListComponent implements OnInit {
+  
+  _listFilter: string;
+
   pageTitle: string = 'Product List Components title';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  products: IProduct[] = [];
   filteredProducts: IProduct[] = [];
-  products: IProduct[] = [
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    }
-  ];
+  
+  constructor(private _productService : ProductService){
+    this.listFilter = '';
+  }  
+
+  ngOnInit(): void {
+    this.products = this._productService.getProducts();
+    this.filteredProducts = this.products;
+  }
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
-  ngOnInit(): void {
-    console.log('from product-lists.ts: In OnInit');
-  }
-
-  _listFilter: string;
-  
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
-  }
-  get listFilter(): string {
-      return this._listFilter;
-  }
-  set listFilter(value: string) {
-      this._listFilter = value;
-      this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-      
-  }
+    
   performFilter(filterBy: string): IProduct[] {
     filterBy = filterBy.toLocaleLowerCase();
     return this.products.filter((product: IProduct) =>
