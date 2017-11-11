bf8f6c2a8b875474aa5838cc3118585879b61101
Geogri Manov <georgimanov@gmail.com>
2017-11-11 12:41:13 +0200

    added HttpClientModule

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,6 +11,7 @@ export class ProductListComponent implements OnInit {
   
   _listFilter: string;
 
+  errorMessage: string;
   pageTitle: string = 'Product List Components title';
   showImage: boolean = false;
   products: IProduct[] = [];
@@ -21,8 +22,12 @@ export class ProductListComponent implements OnInit {
   }  
 
   ngOnInit(): void {
-    this.products = this._productService.getProducts();
-    this.filteredProducts = this.products;
+    this._productService.getProducts()
+    .subscribe(products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+    },
+    error => this.errorMessage = <any>error);
   }
 
   get listFilter(): string {
