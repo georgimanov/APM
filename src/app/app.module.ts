69d351e39ed97b7110ac2448372a6015b3f86fe3
Geogri Manov <georgimanov@gmail.com>
2017-11-18 12:11:42 +0200

    added page-not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './home/welcome.component';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { PageNotFoundComponent } from './products/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +19,21 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        { path: 'products', component: ProductListComponent },
+        { path: 'products/:id', component: ProductDetailComponent },
+        { path: 'welcome', component: WelcomeComponent },
+        { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+        { path: '**', component: PageNotFoundComponent }
+    ], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
